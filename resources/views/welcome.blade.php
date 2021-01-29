ae5c99e72eea220de2a82e2c024a251b9b091e35
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2021-01-28 22:34:09 -0600

    test git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Call Center
+                    Call Center Sys
                 </div>
 
             </div>
